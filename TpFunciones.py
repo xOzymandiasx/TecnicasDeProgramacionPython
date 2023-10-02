22c6a8670982059ae9fe991ad80f9e8b42e077b0
Sergio <sergioottati@hotmail.com>
2023-10-02 11:11:27 -0300

    Ejercicio 4 tecnicamente finalizado

--- TpFunciones.py
+++ TpFunciones.py
@@ -51,45 +51,60 @@
 # print(f"El volumen total en litros de la caja es de: {volumeAndSurface(boxLen, boxHigh, boxWidth)[0]}");
 # print(f"La superficie total en cm² de la caja es de: {volumeAndSurface(boxLen, boxHigh, boxWidth)[1]}");
 
-# * 3) El método más fácil para encriptar un mensaje consiste en el de desplazamiento, cada carácter se desplaza
-# * una cantidad fija llamada clave. Por ejemplo si la clave es 1, la palabra HOLA sería IPMB. Realizar 2
-# * funciones, una para encriptar y otra para desencriptar. A la función se le pasa el texto y la clave (un
-# * entero), y debe devolver un texto. Una pista, la función ord() convierte un carácter en un su código ASCII y
-# * la función chr() convierte el código ASCII en carácter. Tener en cuenta que el texto resultante debe estar
-# * compuesto por las letras del abecedario. Pueden ignorar las diferencias entre mayúsculas y minúsculas, y
-# * pueden ignorar de convertir los signos de puntuación. Opcional, pueden hacer el encriptado más
-# * complejo. Opcional, pueden hacer una sola función que encripte y desencripte, a la misma se le pasa el
-# * texto, la clave (igual que entes) y la operación que se desea realizar (encriptar o desencriptar).
+# # * 3) El método más fácil para encriptar un mensaje consiste en el de desplazamiento, cada carácter se desplaza
+# # * una cantidad fija llamada clave. Por ejemplo si la clave es 1, la palabra HOLA sería IPMB. Realizar 2
+# # * funciones, una para encriptar y otra para desencriptar. A la función se le pasa el texto y la clave (un
+# # * entero), y debe devolver un texto. Una pista, la función ord() convierte un carácter en un su código ASCII y
+# # * la función chr() convierte el código ASCII en carácter. Tener en cuenta que el texto resultante debe estar
+# # * compuesto por las letras del abecedario. Pueden ignorar las diferencias entre mayúsculas y minúsculas, y
+# # * pueden ignorar de convertir los signos de puntuación. Opcional, pueden hacer el encriptado más
+# # * complejo. Opcional, pueden hacer una sola función que encripte y desencripte, a la misma se le pasa el
+# # * texto, la clave (igual que entes) y la operación que se desea realizar (encriptar o desencriptar).
 
 # word = input("Ingrese palabra a encriptar o desencriptar: ");
+# value = int(input("ingrese la clave: "));
 
-def encriptedWord(word, key):
-  encriptedArray = []; 
+# def encriptedWord(word, key):
+#   encriptedArray = []; 
   
-  for letter in range(len(word)):
-    encriptedArray.append(chr(ord(word[letter]) + key));  
+#   for letter in range(len(word)):
+#     encriptedArray.append(chr(ord(word[letter]) + key));  
      
-  return "".join(encriptedArray);
+#   return "".join(encriptedArray);
   
-def desencriptWord(word, key=0):
-  desencriptedArray = [];
+# def desencriptWord(word, key=0):
+#   desencriptedArray = [];
   
-  for letter in range(len(word)):
-    desencriptedArray.append(chr(ord(word[letter]) - key));
+#   for letter in range(len(word)):
+#     desencriptedArray.append(chr(ord(word[letter]) - key));
     
-  return "".join(desencriptedArray);
+#   return "".join(desencriptedArray);
   
-def encriptOrDesencriptWord(word, key):
-  condition = input("Ingrese '1' para encriptar, '2' para desencriptar: ");
+# def encriptOrDesencriptWord(word, key):
+#   condition = input("Ingrese '1' para encriptar, '2' para desencriptar: ");
   
-  if (condition == "1"): return encriptedWord(word, key);
-  if (condition == "2"): return print(desencriptWord(word, key))
-  else: encriptOrDesencriptWord(word, key);
+#   if (condition == "1"): return encriptedWord(word, key);
+#   if (condition == "2"): return desencriptWord(word, key);
+#   else: 
+#     print(f"ingreso de datos incorrecto: '{condition}'");
+#     encriptOrDesencriptWord(word, key);
 
-  
-  
-encriptOrDesencriptWord("ipmb", 1);
+# encriptOrDesencriptWord(word, value);
   
 
+# * 4) Construir una función que reciba el primer nombre, el segundo nombre y apellido de una persona y
+# * devuelva un string con Apellido, Primer nombre inicial del segundo nombre punto. Tener en cuenta que el
+# * segundo nombre es opcional en ese caso no devuelve la inicial del segundo nombre. Opcional, convertir a
+# * mayúscula la inicial de cada nombre y apellido.
 
+# name1 = input("ingrese su nombre: ").capitalize();
+name2 = input("ingrese, si posee, su segundo nombre: ").capitalize();
+# surname = input("ingrese su apellido: ").capitalize();
+
+def completeStringName(name, name2 ="", surname=""):
+  if (len(name2) == 0): return f"holaaaa {name} todo eeee {surname}";
+  if (len(name2) >= 1): return f"holaaaa {name} todo {name2[0]}. eeee {surname}";
   
+
+print(len(name2));
+print(completeStringName("Sergio", name2, "ggg"));
