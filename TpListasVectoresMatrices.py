29848d0e511a8a3723addf49a9b7228b30badd35
Sergio <sergioottati@hotmail.com>
2023-10-13 11:04:19 -0300

    Terminados ejercicios 1 y 4

--- TpListasVectoresMatrices.py
+++ TpListasVectoresMatrices.py
@@ -1,3 +1,28 @@
+#  # * 1) Realizar una función para normalizar un vector. La función recibe un vector de longitud indeterminada de float, y debe devolver un nuevo vector normalizado, es decir el módulo del vector debe ser 1. 
+
+# from math import sqrt;
+
+# vector1 = [1, 1, 1];
+# vector2 = [1, 3, 7, 9];
+
+# def normalizeVector(array):
+#   unitVector = 0;
+#   result = [];
+  
+#   for number in array:
+#     unitVector += number ** 2;
+   
+#   unitVector = sqrt(unitVector);
+#   if (unitVector == 0): return f"Imposible dividir por 0";
+    
+#   for number in array:
+#     result.append(number / unitVector);
+    
+#   return result;
+  
+# print(normalizeVector(vector2));
+
+
 #  # * 2) Construir una función que elimine los elementos duplicados en una lista. La función recibe una lista, y debe devolver una lista que contenga una sola copia de cada elemento de la lista suministrada.
  
 # lista1 = [1, 4, 1, 5, 4, 1];
@@ -28,22 +53,29 @@
 # displayTaTeTi(tablero);
   
 
-# * 4) Realizar una función para buscar si hay ganador en un juego de ta-te-ti. La función recibe una matriz de 3x3 con la situación actual del juego. Cada elemento de la matriz contiene “x”, “o” o “ ”(espacio), igual que en el ejercicio 3. La función debe devolver un None si no hay ganador, o devolver “x” o “o” según si el ganador es la “x” o la “o”.
+# # * 4) Realizar una función para buscar si hay ganador en un juego de ta-te-ti. La función recibe una matriz de 3x3 con la situación actual del juego. Cada elemento de la matriz contiene “x”, “o” o “ ”(espacio), igual que en el ejercicio 3. La función debe devolver un None si no hay ganador, o devolver “x” o “o” según si el ganador es la “x” o la “o”.
 
-tablero3 = [[" "," ","o"],["o","o","o"],["x"," ","x"]];  
+# tablero1 = [["x","o"," "],["o"," ","x"],["x"," ","o"]];
+# tablero2 = [["x","o"," "],["x","o","x"],["x"," ","o"]];
+# tablero3 = [[" "," "," "],["o","x","o"],["x"," ","x"]];
 
-def checkForWinner(board):
-  # Horizontal
-  if (board[0][0] == board[0][1] == board[0][2]): return board[0][0];
-  elif (board[1][0] == board[1][1] == board[1][2]): return board[1][0];
-  elif (board[2][0] == board[2][1] == board[2][2]): return board[2][0];
-  # Vertical
-  elif (board[0][0] == board[1][0] == board[2][0]): return board[0][0];
-  elif (board[0][1] == board[1][1] == board[2][1]): return board[0][1];
-  elif (board[0][2] == board[1][2] == board[2][2]): return board[0][2];
-  # Diagonal
-  elif (board[0][0] == board[1][1] == board[2][2]) \
-    or (board[0][2] == board[1][1] == board[2][0]): return 2;
+# def checkForWinner(board):
+#   # Horizontal
+#   if (board[0][0] == board[0][1] == board[0][2] and board[0][0] != " "): return board[0][0];
+#   elif (board[1][0] == board[1][1] == board[1][2] and board[1][0] != " "): return board[1][0];
+#   elif (board[2][0] == board[2][1] == board[2][2] and board[2][0] != " "): return board[2][0];
+#   # Vertical
+#   elif (board[0][0] == board[1][0] == board[2][0] and board[0][0] != " "): return board[0][0];
+#   elif (board[0][1] == board[1][1] == board[2][1] and board[0][1] != " "): return board[0][1];
+#   elif (board[0][2] == board[1][2] == board[2][2] and board[0][2] != " "): return board[0][2];
+#   # Diagonal
+#   elif (board[0][0] == board[1][1] == board[2][2] and board[0][0] != " "): return board[0][0];
+#   elif (board[0][2] == board[1][1] == board[2][0] and board[0][2] != " "): return board[0][2];
+#   # Si no hay ganador
+#   return None;
   
   
-print(f"el ganador es {checkForWinner(tablero3)}");
+# print(f"el ganador es {checkForWinner(tablero1)}");
+
+
+# * 5) Construir una función para calcular la superficie de un polígono cualquiera, dados sus vértices utilizando la fórmula del área de Gauss. La función recibe una lista de vértices, la cantidad de vértices es indeterminada. Cada vértice es un vector de dos elementos con la posición (x, y). La función debe devolver un float con la superficie del polígono.
